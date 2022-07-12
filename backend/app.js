2d4d7c3697719c7eb963fa44efc96a6e8f43add6
s-gusterev <copywriter-sg@yandex.ru>
2022-07-12 16:56:50 +0300

    crash-test

--- backend/app.js
+++ backend/app.js
@@ -21,6 +21,12 @@ app.use(cors);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.post('/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
